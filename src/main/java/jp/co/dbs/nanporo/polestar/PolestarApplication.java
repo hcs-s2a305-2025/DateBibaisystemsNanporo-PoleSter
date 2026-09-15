e89fb6225c37177fa49e731191f8328438f5703e
石田　陽 <20243003-ishidaharu@hcs.ac.jp>
2026-09-15 13:51:31 +0900

    fifth commit

---
+++ src/main/java/jp/co/dbs/nanporo/polestar/PolestarApplication.java
@@ -0,0 +1,13 @@
+package jp.co.dbs.nanporo.polestar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolestarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolestarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/dbs/nanporo/polestar/PolestarApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.dbs.nanporo.polestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
