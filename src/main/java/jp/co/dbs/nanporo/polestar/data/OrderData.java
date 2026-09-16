e63043a6749c71b7a29737babf5e2308da77c392
廣島　暖士 <20243106-hiroshimaatsushi@hcs.ac.jp>
2026-09-16 11:21:46 +0900

    スキーマのDATETIMEはtimestampに化け新たにOrderDataとOrderEntityが仲間に加わった

--- src/main/java/jp/co/dbs/nanporo/polestar/data/OrderData.java
+++ src/main/java/jp/co/dbs/nanporo/polestar/data/OrderData.java
@@ -1,5 +1,35 @@
 package jp.co.dbs.nanporo.polestar.data;
 
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+// 注文トラン情報
+@Data
 public class OrderData {
     
+    // 注文ID
+    private int orderId;
+
+    // 注文番号
+    private String orderNumber;
+
+    // 受け取り日時
+    private Timestamp getTime;
+
+    // メールアドレス
+    private String mail;
+
+    // 登録日時
+    private Timestamp registerTime;
+
+    // 合計金額
+    private int sumMoney;
+
+    // メモ
+    private String memo;
+
+    // ステータス
+    private String status;
 }
